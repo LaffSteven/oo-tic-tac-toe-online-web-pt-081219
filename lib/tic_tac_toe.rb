9e560c7680e43483186815a5fe5e43d753aa886d
Steven Laff <steve.laff@gmail.com>
2019-09-19 22:19:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,10 +43,12 @@ class TicTacToe
   def turn_count
     number_of_turns = 0
     @board.each do |space|
-    if space == "X" || space == "O"
-        number_of_turns += 1
+      if space == "X" || space == "O"
+          number_of_turns += 1
+      end
     end
-    number_of_turns
   end
-
+  
+  
+  
 end
